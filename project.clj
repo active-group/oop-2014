918e9a1e5540dde83d685c9e8babe4e0f06a210c
Mike Sperber <sperber@deinprogramm.de>
2014-01-31 16:27:30 +0100

    Some info in project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject oop-2014 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Code for OOP 2014 talk on EDSLs in Clojure"
+  :url "https://github.com/active-group/oop-2014"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
